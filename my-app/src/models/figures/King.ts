0580f7ce6222f6c3a80e881a02fd4419c674f1a1
GerasiMandMumu <konst.02@yandex.ru>
2022-05-16 23:29:50 +0300

    Создание проекта

---
+++ my-app/src/models/figures/King.ts
@@ -0,0 +1,20 @@
+import { Figure, FiguresNames } from "./Figure";
+import blackLogo from '../../assets/black-king.png';
+import whiteLogo from '../../assets/white-king.png';
+import { Cell } from "../Cell";
+import { Colors } from "../Colors";
+
+export class King extends Figure{
+    constructor(color: Colors, cell: Cell){
+        super(color, cell);
+        this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
+        this.name = FiguresNames.KING;
+    }
+
+    canMove(target: Cell): boolean {
+        if(!super.canMove(target)){
+            return false;
+        }
+        return true;
+    }
+}
